ce3af129fa626c69dd883dc2adcb163666bd6a4b
Piyush Malhotra <piyushmalhotra28@gmail.com>
2017-12-20 00:34:52 +0530

    Add BatchNormalization

--- model.py
+++ model.py
@@ -63,7 +63,14 @@ def model():
 
     A2 = tf.nn.max_pool(A1, ksize=(1, 5, 5, 1), strides=(1, 5, 5, 1),
                         padding='SAME', name='pool_1_a')
+    m_1, v_1 = tf.nn.moments(A2, axes=[0, 1, 2], keep_dims=False)
+    beta_1 = tf.zeros(A2.shape.as_list()[3])
+    gamma_1 = tf.ones(A2.shape.as_list()[3])
+    A2_bn = tf.nn.batch_normalization(A2, mean=m_1, variance=v_1,
+                                      offset=beta_1, scale=gamma_1,
+                                      variance_epsilon=0.0001)
     params_dict['pool_1_a_out'] = A2
+    params_dict['bn_1a'] = A2_bn
 
     # layer 2
     A3, params_dict['conv_1b'] = conv_layer(A2, 32, 64, 1, 'b')
@@ -71,7 +78,14 @@ def model():
 
     A4 = tf.nn.max_pool(A3, ksize=(1, 5, 5, 1), strides=(1, 5, 5, 1),
                         padding='SAME', name='pool_1_b')
+    m_2, v_2 = tf.nn.moments(A4, axes=[0, 1, 2], keep_dims=False)
+    beta_2 = tf.zeros(A4.shape.as_list()[3])
+    gamma_2 = tf.ones(A4.shape.as_list()[3])
+    A4_bn = tf.nn.batch_normalization(A4, mean=m_2, variance=v_2,
+                                      offset=beta_2, scale=gamma_2,
+                                      variance_epsilon=0.0001)
     params_dict['pool_1_b_out'] = A4
+    params_dict['bn_1b'] = A4_bn
 
     # layer 3
     A5, params_dict['conv_1c'] = conv_layer(A4, 64, 128, 1, 'c')
@@ -82,32 +96,35 @@ def model():
     params_dict['pool_1_c_out'] = A6
 
     # layer 4
-    A7, params_dict['conv_1d'] = conv_layer(A6, 128, 64, 1, 'd')
+    A7, params_dict['conv_1d'] = conv_layer(A6, 128, 256, 1, 'd')
     params_dict['conv_1d_out'] = A7
+    A8 = tf.nn.max_pool(A7, ksize=(1, 5, 5, 1), strides=(1, 5, 5, 1),
+                        padding='SAME', name='pool_1_d')
+    params_dict['pool_1_d'] = A8
 
-    # layer 3
-    A8, params_dict['conv_1e'] = conv_layer(A7, 64, 32, 1, 'e')
+    # layer 5
+    A8, params_dict['conv_1e'] = conv_layer(A7, 256, 512, 1, 'e')
     params_dict['conv_1e_out'] = A8
 
     A9 = tf.nn.max_pool(A8, ksize=(1, 5, 5, 1), strides=(1, 5, 5, 1),
                         padding='SAME', name='pool_1_e')
     params_dict['pool_1_e_out'] = A9
 
-    # layer 5
+    # layer 6
     A10 = flatten(A9)
     params_dict['flatten_out'] = A10
 
-    # layer 6
-    A11, params_dict['dense_2a'] = dense_layer(A10, 1*1*32, 1024, 2, 'a')
+    # layer 7
+    A11, params_dict['dense_2a'] = dense_layer(A10, 1*1*512, 1024, 2, 'a')
     A12 = tf.nn.dropout(A11, keep_prob=0.8)
     params_dict['dense_2a_out'] = A11
     params_dict['dropout_2a'] = A12
 
-    # layer 7
+    # layer 8
     A13, params_dict['dense_2b'] = dense_layer(A12, 1024, 1024, 2, 'b')
     params_dict['dense_2b_out'] = A13
 
-    # layer 8
+    # layer 9
     Z, params_dict['dense_2c'] = dense_layer_wo_non_lin(A12, 1024, 1, 2, 'c')
     return Z, params_dict
 
