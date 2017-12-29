96d910a56d25dfb16e904a072a9ba7e51771398b
Piyush Malhotra <piyushmalhotra28@gmail.com>
2017-12-29 14:44:35 +0530

    Update Generator

--- datalab.py
+++ datalab.py
@@ -8,6 +8,7 @@ class DataLabTrain(object):
         self.path = path
         self.load_at_a_time = load_at_a_time
         self.li_batch_files = os.listdir(path)
+        self.max_len = len(self.li_batch_files)
         np.random.shuffle(self.li_batch_files)
         self.cur_index = 0
 
@@ -16,7 +17,7 @@ class DataLabTrain(object):
         Y = []
         for i in range(self.cur_index, self.cur_index + self.load_at_a_time):
             if i == len(self.li_batch_files):
-                self.cur_index = -self.load_at_a_time
+                self.cur_index = i - self.load_at_a_time
                 break
 
             file = np.load(self.path+self.li_batch_files[i])
@@ -28,7 +29,7 @@ class DataLabTrain(object):
         return X, Y
 
     def generator(self):
-        while 1:
+        while self.cur_index != self.max_len:
             X, Y = self._load_files()
             for i in range(len(X)):
                 yield X[i] / 255, Y[i]
@@ -37,19 +38,19 @@ class DataLabTrain(object):
 class DataLabTest(object):
 
     def __init__(self, path, load_at_a_time=5):
-        self.path = path
+        self.path = path + 'test_'
         self.load_at_a_time = load_at_a_time
-        self.li_batch_files = os.listdir(path)
+        self.max_len = len(os.listdir(path))
         self.cur_index = 0
 
     def _load_files(self):
         X = []
         for i in range(self.cur_index, self.cur_index + self.load_at_a_time):
-            if i == len(self.li_batch_files):
-                self.cur_index = -self.load_at_a_time
+            if i == self.max_len:
+                self.cur_index = i - self.load_at_a_time
                 break
 
-            arr = np.load(self.path+self.li_batch_files[i])
+            arr = np.load(self.path + str(i) + '.npy')
             X.append(arr)
 
         self.cur_index += self.load_at_a_time
@@ -57,7 +58,7 @@ class DataLabTest(object):
         return X
 
     def generator(self):
-        while 1:
+        while self.cur_index != self.max_len:
             X = self._load_files()
             for i in range(len(X)):
                 yield X[i] / 255
